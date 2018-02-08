ae9bd0bc6da732a4b112066fcf9b89f800d2fa13
Nyanfaker <nyanko.anon@gmail.com>
2018-02-08 12:23:33 +0600

    Add project

---
+++ Assets/Enter-Exit/RCC_EnterExitCar.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Vehicles.Car;
+using UnityStandardAssets.Cameras;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class RCC_EnterExitCar : MonoBehaviour {
+
+	private CarController carController;
+	private GameObject carCamera;
+	private GameObject player;
+	public Transform getOutPosition;
+
+	public bool isPlayerIn = false;
+	private bool  opened = false;
+	private float waitTime = 1f;
+	private bool  temp = false;
+
+	private GetInOutCar getInOutCar;
+
+	void Awake (){
+
+		carController = GetComponent<CarController>();
+		carCamera = GameObject.FindObjectOfType<AutoCam>().gameObject;
+
+		if(!getOutPosition){
+			GameObject getOutPos = new GameObject("Get Out Position");
+			getOutPos.transform.SetParent(transform);
+			getOutPos.transform.localPosition = new Vector3(-1.5f, 0f, 0f);
+			getOutPos.transform.localRotation = Quaternion.identity;
+			getOutPosition = getOutPos.transform;
+		}
+
+		getInOutCar = FindObjectOfType<GetInOutCar> ();
+	}
+	
+	void Update (){
+
+		if(getInOutCar.clicked && opened && !temp){
+			GetOut();
+			opened = false;
+			temp = false;
+		}
+
+		if(!isPlayerIn)
+			carController.GetComponent<CarUserControl>().enabled = false;
+
+		if (handBrake > 0f)
+			carController.Move (0f,0f,brake,handBrake);
+
+	}
+	
+	IEnumerator Act (GameObject fpsplayer){
+		
+		player = fpsplayer;
+
+		if (!opened && !temp){
+			GetIn();
+			opened = true;
+			temp = true;
+			yield return new WaitForSeconds(waitTime);
+			temp = false;
+		}
+
+	}
+	
+	void GetIn (){
+
+		isPlayerIn = true;
+
+		carCamera.SetActive(true);
+
+		carCamera.transform.GetComponent<AutoCam>().SetTarget(transform);
+		player.transform.SetParent(transform);
+		player.transform.localPosition = Vector3.zero;
+		player.transform.localRotation = Quaternion.identity;
+		player.SetActive(false);
+
+		carController.GetComponent<CarUserControl>().enabled = true;
+		
+		Cursor.lockState = CursorLockMode.None;
+
+	}
+	
+	void GetOut (){
+
+		isPlayerIn = false;
+
+		player.transform.SetParent(null);
+		player.transform.position = getOutPosition.position;
+		player.transform.rotation = getOutPosition.rotation;
+		player.transform.rotation = Quaternion.Euler (0f, player.transform.eulerAngles.y, 0f);
+		carCamera.SetActive(false);
+		player.SetActive(true);
+
+		StartCoroutine("BrakeGetOut");
+
+		carController.GetComponent<CarUserControl>().enabled = false;
+		
+		Cursor.lockState = CursorLockMode.Locked;
+
+	}
+
+	float brake;
+	float handBrake;
+
+	IEnumerator BrakeGetOut() {
+		brake = -1f;
+		handBrake = 1f;
+		yield return new WaitForSeconds (1f);
+		brake = 0f;
+		yield return new WaitForSeconds (4f);
+		handBrake = 0f;
+		carController.Move (0f,-1f,-1f,0f);
+		carController.Move (0f,0f,0f,0f);
+	}
+	
+}
---
+++ Assets/Enter-Exit/RCC_EnterExitPlayer.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Vehicles.Car;
+using UnityStandardAssets.Cameras;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class RCC_EnterExitPlayer : MonoBehaviour {
+
+	public PlayerType playerType;
+	public enum PlayerType{FPS, TPS}
+
+	public GameObject rootOfPlayer;
+	public float maxRayDistance= 2f;
+	public float rayHeight = 1f;
+	public GameObject TPSCamera;
+	private bool showGui = false;
+
+	private GetInOutCar getInOutCar;
+
+	void Start(){
+
+		if (!rootOfPlayer)
+			rootOfPlayer = transform.root.gameObject;
+
+		GameObject carCamera = GameObject.FindObjectOfType<AutoCam>().gameObject;
+		carCamera.SetActive(false);
+
+		getInOutCar = FindObjectOfType<GetInOutCar> ();
+	}
+	
+	void Update (){
+		
+		Vector3 direction= transform.TransformDirection(Vector3.forward);
+		RaycastHit hit;
+
+		if(Physics.Raycast(new Vector3(transform.position.x, transform.position.y + (playerType == PlayerType.TPS ? rayHeight : 0f), transform.position.z), direction, out hit, maxRayDistance)){
+
+			if(hit.transform.GetComponentInParent<RCC_EnterExitCar>()){
+
+				showGui = true;
+
+				if ( getInOutCar.clicked) {
+
+					hit.transform.GetComponentInParent<CarController> ().SendMessage ("Act", rootOfPlayer, SendMessageOptions.DontRequireReceiver);
+					
+				}
+
+			}else{
+
+				showGui = false;
+
+			}
+			
+		}else{
+
+			showGui = false;
+
+		}
+
+			getInOutCar.gameObject.SetActive (showGui);
+		
+	}
+
+	void OnDrawGizmos(){
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawRay (new Vector3(transform.position.x, transform.position.y + (playerType == PlayerType.TPS ? rayHeight : 0f), transform.position.z), transform.forward * maxRayDistance);
+		
+	}
+
+	void OnEnable(){
+
+		if (TPSCamera)
+			TPSCamera.SetActive (true);
+
+	}
+
+	void OnDisable(){
+
+		if (TPSCamera)
+			TPSCamera.SetActive (false);
+
+	}
+	
+}
